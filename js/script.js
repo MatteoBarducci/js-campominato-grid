ebc65f88559aefe58bb5718e39089e7dbcb8cfdd
MatteoBarducci <mbardu1301@gmail.com>
2024-03-18 09:00:29 +0100

    feat: add function to generate a new square

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+const grid = document.querySelector('.grid')
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function(){
+    for(let i = 1; i <= 100; i++){
+        const newSquare = generateSquare(i);
+        grid.append(newSquare);
+    }
+})
+
+function generateSquare(number){
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    newSquare.innerHTML = '<span>' + number + '</span>';
+    return newSquare
+}
